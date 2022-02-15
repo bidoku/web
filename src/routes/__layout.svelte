6003ba246ffb542ccb90cb7e5df273c13ea6f1a1
Furkan Demir <frndemir@gmail.com>
2022-02-15 22:30:40 +0300

    :recycle: refactor firebase integration and impl. auth

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,14 +1,17 @@
 <script lang="ts">
+	import 'carbon-components-svelte/css/white.css';
+
 	import { onMount } from 'svelte';
-	import { initFirebase } from '$lib/initFirebase';
-	import Header from '../components/layout/Header.svelte';
 	import { getAuth as auth } from 'firebase/auth';
+	import '$lib/firebase';
+
+	import Header from '../components/layout/Header.svelte';
+	import Footer from '../components/layout/Footer.svelte';
+	import { Content } from 'carbon-components-svelte';
 
 	import { user, loggedIn } from '../store/authStore';
 
 	onMount(() => {
-		initFirebase();
-
 		auth().onAuthStateChanged((authenticatedUser) => {
 			loggedIn.set(user !== null);
 			user.set(authenticatedUser);
@@ -16,5 +19,12 @@
 	});
 </script>
 
+<svelte:head>
+	<title>Bidoku</title>
+</svelte:head>
+
 <Header />
-<slot />
+<Content>
+	<slot />
+</Content>
+<Footer />
